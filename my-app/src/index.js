fac30522d89f55386ad7ec578a06ec0df6f5efe7
takashabe <takashabe.public@gmail.com>
2017-09-09 00:58:26 +0900

    Add judgement of the winner

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -38,6 +38,10 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    // already finished game, or already filled squares
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.getPlayer();
     this.setState({
       squares: squares,
@@ -46,7 +50,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + this.getPlayer();
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + this.getPlayer();
+    }
 
     return (
       <div>
@@ -69,6 +79,26 @@ class Board extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 class Game extends React.Component {
