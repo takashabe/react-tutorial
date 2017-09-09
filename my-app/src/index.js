9e335accfa4917c5e26ef5826969681d1965b1b7
takashabe <takashabe.public@gmail.com>
2017-09-09 20:24:57 +0900

    Fix href point #

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -122,7 +122,7 @@ class Game extends React.Component {
       const desc = move ? 'Move #' + move : 'Game start';
       return (
         <li key={move}>
-          <a href="#" onClick={() => this.jumpTo(move)}>
+          <a href="#moveHistory" onClick={() => this.jumpTo(move)}>
             {desc}
           </a>
         </li>
