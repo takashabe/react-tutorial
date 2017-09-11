051f748c5c54a5d671647d21ce9b4a405789f2fa
takashabe <takashabe.public@gmail.com>
2017-09-12 00:22:12 +0900

    Add highlight line for factor of won

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -12,10 +12,18 @@ function Square(props) {
 
 class Board extends React.Component {
   renderSquare(i, j) {
+    let value = this.props.squares[i][j];
+    if (this.props.highlightLine) {
+      for (let o of this.props.highlightLine) {
+        if (o.x === i && o.y === j) {
+          value = <mark>{value}</mark>;
+        }
+      }
+    }
     return (
       <Square
         key={i + '_' + j}
-        value={this.props.squares[i][j]}
+        value={value}
         onClick={() => this.props.onClick(i, j)}
       />
     );
@@ -36,46 +44,6 @@ class Board extends React.Component {
     }
     return <div>{rows}</div>;
   }
-}
-
-function calculateWinner(squares) {
-  // check x lines
-  for (let i = 0; i < 3; i++) {
-    if (
-      squares[i][0] &&
-      squares[i][0] === squares[i][1] &&
-      squares[i][0] === squares[i][2]
-    ) {
-      return squares[(i, 0)];
-    }
-  }
-  // check y lines
-  for (let i = 0; i < 3; i++) {
-    if (
-      squares[0][i] &&
-      squares[0][i] === squares[1][i] &&
-      squares[0][i] === squares[2][i]
-    ) {
-      return squares[0][i];
-    }
-  }
-  // check slanting lines
-  if (
-    squares[0][0] &&
-    squares[0][0] === squares[1][1] &&
-    squares[0][0] === squares[2][2]
-  ) {
-    return squares[0][0];
-  }
-  if (
-    squares[2][0] &&
-    squares[2][0] === squares[1][1] &&
-    squares[2][0] === squares[0][2]
-  ) {
-    return squares[2][0];
-  }
-
-  return null;
 }
 
 class Game extends React.Component {
@@ -110,7 +78,7 @@ class Game extends React.Component {
     }
 
     // already finished game, or already filled squares
-    if (calculateWinner(squares) || squares[i][j]) {
+    if (calculateResult(squares).winner || squares[i][j]) {
       return;
     }
     squares[i][j] = this.getPlayer();
@@ -141,7 +109,7 @@ class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
+    const result = calculateResult(current.squares);
     const toggle = (
       <button onClick={() => this.handleToggle(this.state.historyOrder)}>
         Toggle
@@ -170,8 +138,8 @@ class Game extends React.Component {
       <ol reversed>{moves}</ol>
     );
 
-    const status = winner
-      ? 'Winner: ' + winner
+    const status = result.winner
+      ? 'Winner: ' + result.winner
       : 'Next player: ' + this.getPlayer();
 
     return (
@@ -180,6 +148,7 @@ class Game extends React.Component {
           <Board
             squares={current.squares}
             onClick={(i, j) => this.handleClick(i, j)}
+            highlightLine={result.line}
           />
         </div>
         <div className="game-info">
@@ -195,3 +164,58 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(<Game />, document.getElementById('root'));
+
+function calculateResult(squares) {
+  // check x lines
+  for (let i = 0; i < 3; i++) {
+    if (
+      squares[i][0] &&
+      squares[i][0] === squares[i][1] &&
+      squares[i][0] === squares[i][2]
+    ) {
+      return {
+        winner: squares[i][0],
+        line: [{x: i, y: 0}, {x: i, y: 1}, {x: i, y: 2}],
+      };
+    }
+  }
+  // check y lines
+  for (let i = 0; i < 3; i++) {
+    if (
+      squares[0][i] &&
+      squares[0][i] === squares[1][i] &&
+      squares[0][i] === squares[2][i]
+    ) {
+      return {
+        winner: squares[0][i],
+        line: [{x: 0, y: i}, {x: 1, y: i}, {x: 2, y: i}],
+      };
+    }
+  }
+  // check slanting lines
+  if (
+    squares[0][0] &&
+    squares[0][0] === squares[1][1] &&
+    squares[0][0] === squares[2][2]
+  ) {
+    return {
+      winner: squares[0][0],
+      line: [{x: 0, y: 0}, {x: 1, y: 1}, {x: 2, y: 2}],
+    };
+  }
+  if (
+    squares[2][0] &&
+    squares[2][0] === squares[1][1] &&
+    squares[2][0] === squares[0][2]
+  ) {
+    return {
+      winner: squares[2][0],
+      line: [{x: 2, y: 0}, {x: 1, y: 1}, {x: 0, y: 2}],
+    };
+  }
+
+  return {
+    winner: null,
+    line: null,
+  };
+}
